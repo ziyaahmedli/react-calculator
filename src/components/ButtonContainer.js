93ea68377dde8837d5954295e65457e73c042dc0
Ziya Ahmedli <ziyaahmedli10@gmail.com>
2022-05-19 23:28:02 +0400

    .

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -2,11 +2,30 @@ import React from 'react'
 import Buttons from './Buttons'
 
 const ButtonContainer = () => {
+
+  const currentButtons = [
+    "C", "%", "re", "/",
+    "7", "8", "9", "*",
+    "4", "5", "6", "-",
+    "1", "2", "3", "+",
+    "0", "000", ".", "="
+  ]
+
+
+  currentButtons.map(button => {
+    button.includes('C')
+  })
+
+  const secondaryButtons = []
+
   return (
     <div className='buttonContainer'>
-      <Buttons 
-        value={4}
-      />
+      {currentButtons.map(button => {
+        return <Buttons 
+        value={button} 
+        // class = { button.includes(["C", "%", "re"])? "blue" : "red"}
+        />
+      })}
     </div>
   )
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Buttons = (props) => {
   return (
-    <button>{props.value}</button>
+    <button className={props.class}>{props.value}</button>
   )
 }
 
